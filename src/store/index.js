f9f8ad36161851e427e5bf2afadd8f4e3352670b
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2024-11-17 08:20:49 +0530

    latest

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { formReducer, changeCost, changeName } from "./slices/formSlice";
+import {
+  carsReducer,
+  changeSearchTerm,
+  addCar,
+  removeCar,
+} from "./slices/carsSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
+
+export { store, changeCost, changeName, changeSearchTerm, addCar, removeCar };
